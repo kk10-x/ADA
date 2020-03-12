0c24f8548ee56b3055b61e823fd535f20edce066
kk10-x <53857387+kk10-x@users.noreply.github.com>
2020-03-12 22:26:56 +0530

    Update jt.cpp

--- jt.cpp
+++ jt.cpp
@@ -18,60 +18,6 @@ struct jt
 	vector<int> val;
 	vector<int> pos;	
 	vector<bool> dir; //its a diff "dir" ->direction
-	int sign_;
- 
-	jt(int n) : val(till(n, 1)), pos(till(n + 1, -1)), dir(n + 1, false), sign_(1) {} //constructor fo the structure
- 
-	int LargestMobile() const //so that it doesnt erase the prev values 	
-	{
-		for (int r = val.size(); r > 0; --r)
-		{
-			const int loc = pos[r] + (dir[r] ? 1 : -1);
-			if (loc >= 0 && loc < val.size() && val[loc] < r)
-				return r;
-		}
-		return 0;
-	}
- 
-	bool comp() const { return LargestMobile() == 0; } //checking is the limit has been reached
- 
-	void operator++() //operator modifiction....the way ++ works in the incrementation	
-	{
-		const int r = LargestMobile();
-		const int rLoc = pos[r];
-		const int lLoc = rLoc + (dir[r] ? 1 : -1);
-		const int l = val[lLoc];
-		// do the swap
-		swap(val[lLoc], val[rLoc]);
-		swap(pos[l], pos[r]);
-		sign_ = -sign_;
-		// change directions
-		for (auto pd = dir.begin() + r + 1; pd != dir.end(); ++pd)
-			*pd = !*pd;
-	}
-};
- 
-int main(void)
-{#include <iostream>
-#include <vector>
-//DONT 
-//DO IT
-//:"/
-using namespace std;
- 
-vector<int> till(int n, int offset = 0)
-{
-	vector<int> baka(n);
-	for (int i= 0; i < n; ++i)
-		baka[i] = i + offset;
-	return baka;
-}
- 
-struct jt
-{
-	vector<int> val;
-	vector<int> pos;	
-	vector<bool> dir; //its a diff "dir" ->direction
 	int sign;
  
 	jt(int n) : val(till(n, 1)), pos(till(n + 1, -1)), dir(n + 1, false), sign(1) {} //constructor fo the structure
@@ -105,18 +51,6 @@ struct jt
  
 int main(void)
 {
-	auto n=0;
-	cout<<"Enter the length of the permutation\n";
-	cin>>n;
-	jt state(n);
-	do
-	{
-		for (auto v : state.val)
-			cout << v << " ";
-		cout << "\n";
-		++state;
-	} while (!state.comp());
-}
 	auto n=0;
 	cout<<"Enter the length of the permutation\n";
 	cin>>n;
