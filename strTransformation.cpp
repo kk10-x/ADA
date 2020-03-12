c21ece8c635b99310a1ded3e459711bbd8e4b576
kk10-x <53857387+kk10-x@users.noreply.github.com>
2020-03-12 23:04:45 +0530

    Update strTransformation.cpp

--- strTransformation.cpp
+++ strTransformation.cpp
@@ -6,17 +6,21 @@ int main()
     string s1,s2;
     cout<<"Enter the String 1 and String 2\n";
     cin>>s1>>s2;
+    int count=0;
     int l1=s1.length();
     int l2=s2.length();
     if(l1<=l2)
         for(int i=0; i<l2;i++)
         {
             if((int)s1[i]!=(int)s2[i])
+            {
                 s2[i]=s1[i];
+                count++;
+            }
         }
     else 
         cout<<"The length of the s1 is greater than s2\n ";
     
-    cout<<"The modified string:"<<s2;
+    cout<<"The modified string:"<<s2<<"Then number changes required is:"<<count;
     return 0;
 }
